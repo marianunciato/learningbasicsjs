9d017e600238f72d65736d82a5064665948cc001
Maria Eduarda Anunciato <122390168+marianunciato@users.noreply.github.com>
2023-03-13 14:03:26 -0300

    Add files via upload

---
+++ javascript/type-boolean.js
@@ -0,0 +1,12 @@
+// boolean
+// true -> verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+let cadastroAtivado = true;
+
+const texto1 = 'Alura';
+const texto2 = 'Alura';
+
+console.log(texto1 === texto2);
